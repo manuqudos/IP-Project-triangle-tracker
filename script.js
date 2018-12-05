1814f81d40f54bbc32e0572ecc718254f361b09c
manuqudos <manu.qudos@gmail.com>
2018-12-05 11:45:56 +0300

    add script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+ function triangleTracker() {
+ var lengthOfSideA = parseInt(document.getElementById("a").value);
+ var lengthOfSideB = parseInt(document.getElementById("b").value);
+ var lengthOfSideC = parseInt(document.getElementById("c").value);
+
+ if (isNaN(lengthOfSideA)|| isNaN(lengthOfSideB)|| isNaN(lengthOfSideC)){
+   alert("Enter a number")
+ }
+ else if (lengthOfSideA === lengthOfSideB && lengthOfSideB === lengthOfSideC && lengthOfSideC === lengthOfSideA) {
+   alert("An Equilateral Triangle");
+ }
+ else if (lengthOfSideA === lengthOfSideB || lengthOfSideA === lengthOfSideC || lengthOfSideB === lengthOfSideC ) {
+   alert("An Isosceles Triangle");
+ }
+ else if ((lengthOfSideA+lengthOfSideB) <= (lengthOfSideC) || (lengthOfSideA+lengthOfSideC) <= (lengthOfSideB) || (lengthOfSideB+c)<= (lengthOfSideA)) {
+   alert("Not a Triangle");
+ }
+ else  {
+   alert("A Scalene Triangle");
+ }
+}
